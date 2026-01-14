915e1da9302d85edf595c1c54a727a79964d17e4
vidhya-th <vidhyath96@gmail.com>
2026-01-14 11:41:58 +0100

    prediction pipiline

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pickle
 import dill
 from sklearn.metrics import r2_score
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     """
@@ -21,14 +22,20 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e)
     
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param_grid = param[list(models.keys())[i]]
+            gs = GridSearchCV(model,param_grid,cv=3) 
+            gs.fit(X_train,y_train)
 
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+
+            
 
             y_train_pred = model.predict(X_train)
 
@@ -42,5 +49,16 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
 
         return report
 
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    """
+    Loads a Python object from the specified file path using dill.
+    """
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return pickle.load(file_obj)
+    
     except Exception as e:
         raise CustomException(e, sys)
